c0a54b605f64509a8934d3b6e9e578ffd3c19954
ashRIDER <ashRIDER@LAPTOP-K322O225>
2025-03-11 17:11:41 +0530

    added sopln

--- SampleTest/src/main/java/sample/utility/SampleTesting.java
+++ SampleTest/src/main/java/sample/utility/SampleTesting.java
@@ -4,6 +4,8 @@ public class SampleTesting {
 
 	public static void main(String[] args) {
 		System.out.println("creted new page");
+		System.out.println("login done");
+		System.out.println("logout done");
 	}
 
 }
